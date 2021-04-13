cb0855c88f036692e5d4221ce7970b8ad0de98c0
Christian Rojas <christianftw@ymail.com>
2021-04-12 17:07:10 -0700

    optimized mobile behaviour and added favicon

--- src/effects/ScrollPageEvent.js
+++ src/effects/ScrollPageEvent.js
@@ -4,6 +4,8 @@
     elem = id of the page element
 */
 const ScrollPageEvent  = (elementId, callBack, prev) => {
+    let start = null;
+
     const elem = document.getElementById(elementId);
 
     const wheelHandler = (e) => {
@@ -22,11 +24,40 @@ const ScrollPageEvent  = (elementId, callBack, prev) => {
             }
         }
     };
+
+    const touchStartHandler = (e) => {
+        start = e.changedTouches[0];
+    };
+
+    const touchEndHandler = (e) => {
+        const offset = elem.scrollTop;
+        const maxPageHeight = (elem.scrollHeight - elem.clientHeight) - 1;
+
+
+        let end = e.changedTouches[0];
+
+        if (offset >= maxPageHeight) {
+            if ((end.screenY - start.screenY) < 0) {
+                console.log("Scrolling down");
+                if (callBack) callBack();
+            }
+        } 
+
+        if (offset === 0) {
+            if ((end.screenY - start.screenY) > 0) {
+                if (prev) prev(); 
+            }
+        }
+    };
     
     elem.addEventListener("wheel", wheelHandler);
+    elem.addEventListener("touchstart", touchStartHandler);
+    elem.addEventListener("touchend", touchEndHandler);
 
     return () => {
         if (document.getElementById(elem)) {
+            elem.removeEventListener("touchmove", touchStartHandler);
+            elem.removeEventListener("touchend", touchEndHandler);
             return elem.removeEventListener("wheel", wheelHandler);
         } else {
             //Handle error
