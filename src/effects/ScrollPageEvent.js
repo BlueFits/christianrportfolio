bea14b17dcc3f4a75563185428ef6d16de763fd4
Christian Rojas <christianftw@ymail.com>
2021-04-15 11:10:36 -0700

    changed slide sensitivity

--- src/effects/ScrollPageEvent.js
+++ src/effects/ScrollPageEvent.js
@@ -13,13 +13,13 @@ const ScrollPageEvent  = (elementId, callBack, prev) => {
         const maxPageHeight = (elem.scrollHeight - elem.clientHeight) - 1;
 
         if (offset >= maxPageHeight) {
-            if (e.deltaY >= 50) {
+            if (e.deltaY >= 90) {
                 if (callBack) callBack();
             } 
         } 
 
         if (offset === 0) {
-            if (e.deltaY <= -50) {
+            if (e.deltaY <= -90) {
                 if (prev) prev(); 
             }
         }
